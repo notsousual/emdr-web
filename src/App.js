f8678367a7bb8f52c57eb4cb55db4d2bf766e83b
notsousual <alisazakharenko@gmail.com>
2023-11-05 21:54:04 +0100

    костыль на оверфлоу

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.scss";
 
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Simulator from "./pages/Simulator";
 import About from "./pages/About";
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    // Check if the current route is '/about'
+    if (location.pathname === "/about") {
+      // Change the overflow on body to 'auto' when on the About page
+      document.body.style.overflow = "auto";
+    } else {
+      // Change the overflow on body to 'hidden' for all other pages
+      document.body.style.overflow = "hidden";
+    }
+
+    // Cleanup function to reset overflow when the component is unmounted or route changes
+    return () => {
+      document.body.style.overflow = "hidden";
+    };
+  }, [location]); // Re-run the effect when the location changes
   return (
     <>
       <Routes>
